08d375dc65fb91eab37e3abbb7c1e10fd17165ed
pavis <pavis_pu@163.com>
2017-10-11 06:04:16 -0700

    hello.c

--- hello.c
+++ hello.c
@@ -18,6 +18,8 @@
 #include <linux/timer.h>
 #include <linux/slab.h>
 #include <linux/errno.h>
+#include <linux/wait.h>
+#include <linux/spinlock.h>
 #include <asm/io.h>
 //#include <asm/system.h>
 #include <asm/atomic.h>
@@ -42,13 +44,16 @@ module_param(dev_major,int,S_IRUGO);
 
 
 struct hello_dev_t{            //define a device struct
-     
+    char *data;
+    unsigned long size;
     struct cdev cdev;          //Initialization struct for device of hello
     
 };
 
 struct hello_dev_t *hello_dev;
 
+
+spinlock_t lock;               //declare a spinlock
 
 
 
@@ -76,15 +81,66 @@ int hello_release(struct inode *inode,struct file *file)
 }
 
 
-static ssize_t hello_read(struct file *filp,char __user *buf,size_t count,loff_t *ppos)
+static ssize_t hello_read(struct file *filp,char __user *buf,size_t size,loff_t *ppos)
 {
+    
+    unsigned long p = *ppos;
+    unsigned int count = size;
+    int ret = 0;
+    
+    struct mem_dev *dev;
+    dev= filp->private_data;
     printk(KERN_NOTICE"====hello_dev read===");
-    return 0;
+    if(p >= hello_dev->size)
+      return 0;
+    if(count > hello_dev-> size-p)
+      count = hello_dev-> size-p;
+    
+    if(copy_to_user(buf,(void *)(hello_dev->data + p),count))
+    {
+        ret = -EFAULT;
+    }
+    else
+    {
+        *ppos += count;
+        ret = count;
+    }
+    return ret;
 }
 
-static ssize_t hello_write(struct file *filp,const char __user *buf,size_t count,loff_t *ppos)
+static ssize_t hello_write(struct file *filp,const char __user *buf,size_t size,loff_t *ppos)
 {
+    
+    unsigned long p = *ppos;
+    unsigned int count = size;
+    int ret = 0;    
+    struct mem_dev *dev;
+    dev = filp->private_data;
     printk(KERN_NOTICE"====hello_dev write===");
+
+    spin_lock(&lock);
+/*************************critical zone******************/
+    if(p >= hello_dev->size)
+      return 0;
+    if(count > hello_dev-> size-p)
+      count = hello_dev-> size-p;
+
+    if(copy_from_user((void *)(hello_dev->data + p),buf,count))
+    {
+        ret = -EFAULT;
+    }
+    else
+    {
+        *ppos += count;
+        ret = count;
+    }
+    return ret;
+
+
+
+/********************************************************/
+    spin_unlock(&lock);
+    
     return 0;
 }
 
@@ -101,50 +157,50 @@ int static hello_init(void)
 {
      int res;
      dev_t devno = MKDEV(dev_major,0); 
-     struct class *helloclass;
+     struct class *helloclass; 
+     spin_lock_init(&lock);                //Initialization a spinlock
 
      printk("<0>""hello world,mine cdev is poweron");
      
-
-                         
-          printk("<0>""=============init1==================");
      if(dev_major)
        res = register_chrdev_region(devno,DEV_DEVS,"hellodev"); //allot device number
      else
      {
-       res = alloc_chrdev_region(&devno,0,DEV_DEVS,"hellodev");
-       dev_major = MAJOR(devno);
+       res = alloc_chrdev_region(&devno,0,DEV_DEVS,"hellodev");//动态分配设备编号
+       dev_major = MAJOR(devno);           //获取主设备号
      }    
      if(res < 0)
          return res;
     
-
-     printk("<0>""=============init2==================");
      hello_dev = kmalloc(DEV_DEVS*sizeof(struct hello_dev_t),GFP_KERNEL);  //allot memory for dev
-     printk("<0>""=============init3==================");
+
      if(!hello_dev)
      {
-         res = -ENOMEM;
+         res = -ENOMEM;               //error num 12
          goto fail_malloc;
      }
-     printk("<0>""=============init4==================");
+  
      memset(hello_dev,0,sizeof(struct hello_dev_t));
      
      cdev_init(&hello_dev->cdev,&hello_dev_fops);        //building connect between in cdev and fops    
-     printk("<0>""=============init5==================");
+ 
      hello_dev->cdev.owner = THIS_MODULE;
      hello_dev->cdev.ops = &hello_dev_fops;
-     printk("<0>""=============init6==================");
+    
      cdev_add(&hello_dev->cdev,MKDEV(dev_major,0),DEV_DEVS);
 
     
      helloclass = class_create(THIS_MODULE,"hello_device");    //creating device file at /dev
      device_create(helloclass,NULL,devno,NULL,"hello_device");
      
+     hello_dev->size = DEV_SIZE;
+     hello_dev->data = kmalloc(hello_dev->size,GFP_KERNEL);
+     memset(hello_dev->data,0,hello_dev->size);
+     
      return 0;
   
      fail_malloc:
-         unregister_chrdev_region(devno,1);
+         unregister_chrdev_region(devno,1);//释放申请的设备号,The number is 1.
      return res;
 }
 
@@ -156,6 +212,7 @@ void static hello_exit(void)
 
       printk("<0>""mineworld is poweroff");
       cdev_del(&hello_dev->cdev);                                   //logout cdev
+      kfree(hello_dev->data);
       kfree(hello_dev);
       unregister_chrdev_region(MKDEV(dev_major,0),1);
 }
